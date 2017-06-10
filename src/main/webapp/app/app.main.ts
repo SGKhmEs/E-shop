fb46a8e12fb91cc94e22a0463a85568f704df268
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-11 02:43:11 +0300

    update generator

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -8,4 +8,6 @@ if (module['hot']) {
     module['hot'].accept();
 }
 
-platformBrowserDynamic().bootstrapModule(EshopAppModule);
+platformBrowserDynamic().bootstrapModule(EshopAppModule)
+.then((success) => console.log(`Application started`))
+.catch((err) => console.error(err));
