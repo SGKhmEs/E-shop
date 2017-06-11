d1edf8c1ad05f07ee368d7eea2dc78c97b9f4c56
Sergey Kostyniuk <sergey7kos@gmail.com>
2017-06-11 12:44:27 +0300

    add CommentsDTO

---
+++ src/main/java/com/social/eshop/service/dto/CommentsDTO.java
@@ -0,0 +1,39 @@
+package com.social.eshop.service.dto;
+
+import com.social.eshop.domain.Customer;
+import com.social.eshop.service.mapper.AutoMapping;
+
+import java.time.ZonedDateTime;
+
+public class CommentsDTO implements AutoMapping {
+
+    private String comments;
+    private ZonedDateTime date;
+    private Customer customer;
+
+    public CommentsDTO() { }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
