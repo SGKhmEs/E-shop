a1f2d052afb6aa4df891cf939b6249133aea6936
Sergey Kostyniuk <sergey7kos@gmail.com>
2017-06-11 20:09:17 +0300

    add CustomerDTO

---
+++ src/main/java/com/social/eshop/service/dto/CustomerDTO.java
@@ -0,0 +1,60 @@
+package com.social.eshop.service.dto;
+
+import com.social.eshop.domain.Products;
+import com.social.eshop.domain.Seen;
+import com.social.eshop.domain.WishList;
+import com.social.eshop.service.mapper.AutoMapping;
+
+import java.util.List;
+
+public class CustomerDTO implements AutoMapping {
+
+    private String sessionId;
+    private List<WishList> wishLists;
+    private List<Seen> seens;
+    private List<Products> products;
+
+    public CustomerDTO() { }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public List<WishList> getWishLists() {
+        return wishLists;
+    }
+
+    public void setWishLists(List<WishList> wishLists) {
+        this.wishLists = wishLists;
+    }
+
+    public List<Seen> getSeens() {
+        return seens;
+    }
+
+    public void setSeens(List<Seen> seens) {
+        this.seens = seens;
+    }
+
+    public List<Products> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Products> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+            "sessionId='" + sessionId + '\'' +
+            ", wishLists=" + wishLists +
+            ", seens=" + seens +
+            ", products=" + products +
+            '}';
+    }
+}
