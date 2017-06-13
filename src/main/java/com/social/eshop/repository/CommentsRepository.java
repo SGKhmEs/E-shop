53b326ac477c8dbcb0443cac2874093cc8ea2fb0
Alexander Torchynskyi <9torchuncky6@gmail.com>
2017-06-13 23:30:54 +0300

    Added CommentsService and CustomerService

--- src/main/java/com/social/eshop/repository/CommentsRepository.java
+++ src/main/java/com/social/eshop/repository/CommentsRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 
+import java.util.List;
+
 
 /**
  * Spring Data JPA repository for the Comments entity.
@@ -12,5 +14,6 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface CommentsRepository extends JpaRepository<Comments,Long> {
-    
+    @Query("FROM Comment where post_id =?1")
+    public List<Comments> findByPostId(Long id);
 }
--- src/main/java/com/social/eshop/service/CommentsDTOService.java
+++ src/main/java/com/social/eshop/service/CommentsDTOService.java
@@ -1,14 +1,63 @@
 package com.social.eshop.service;
 
+import com.social.eshop.domain.Comments;
+import com.social.eshop.repository.CommentsRepository;
+import com.social.eshop.service.dto.CommentsDTO;
+import com.social.eshop.service.dto.CustomerDTO;
+import com.social.eshop.service.dto.ProductsDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
 @Service
 @Transactional
-public class CommentsDTOService {
+public class CommentsDTOService  {
 
     private final Logger log = LoggerFactory.getLogger(CommentsDTOService.class);
+    private final CommentsRepository commentsRepository;
+    private final CustomerDTOService customerDTOService;
+    private  final ProductsDTO productsDTO;
 
+    public CommentsDTOService(CommentsRepository commentsRepository, CustomerDTOService customerDTOService, ProductsDTO productsDTO) {
+        this.commentsRepository = commentsRepository;
+        this.customerDTOService = customerDTOService;
+        this.productsDTO = productsDTO;
+    }
+
+    public CommentsDTO findOne(Long id){
+        log.debug("Request to get Comments : {}", id);
+        Comments comment = commentsRepository.getOne(id);
+        CustomerDTO customerDTO = customerDTOService.findOne(commentsRepository.getOne(id).getCustomer().getId());
+        CommentsDTO commentDTO = new CommentsDTO();
+        try {
+            commentDTO.mappingToDTO(comment, customerDTO);
+        } catch (InvocationTargetException ex) {
+            java.util.logging.Logger.getLogger(CommentsDTOService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return commentDTO;
+    }
+
+    public List<CommentsDTO> findAllByPost(Long id) {
+
+        List<Comments> commentList = commentsRepository.findByPostId(id);
+        List<CommentsDTO> commentDTOList = new ArrayList<>();
+
+        for (Comments comment : commentList) {
+            CommentsDTO commentDTO = new CommentsDTO();
+            CustomerDTO customerDTO = customerDTOService.findOne(commentsRepository.getOne(comment.getId()).getCustomer().getId());
+            try {
+                commentDTO.mappingToDTO(comment, customerDTO);
+            } catch (InvocationTargetException ex) {
+                java.util.logging.Logger.getLogger(CommentsDTOService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            commentDTOList.add(commentDTO);
+        }
+        return commentDTOList;
+    }
 }
