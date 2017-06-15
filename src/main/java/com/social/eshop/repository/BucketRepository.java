f6eacf923d20845060f975d9d4407d39d3227fa4
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-15 18:43:03 +0300

    add methods in BucketDTOService

--- src/main/java/com/social/eshop/repository/BucketRepository.java
+++ src/main/java/com/social/eshop/repository/BucketRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 
+import java.util.List;
+
 
 /**
  * Spring Data JPA repository for the Bucket entity.
@@ -12,5 +14,6 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface BucketRepository extends JpaRepository<Bucket,Long> {
-    
+    @Query("FROM Bucket where products_id =?1")
+    public List<Bucket> findByProductsId(Long id);
 }
