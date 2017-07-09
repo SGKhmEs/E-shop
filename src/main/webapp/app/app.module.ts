0b705551e459331ce89a54f5569502f3c8921322
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-07-10 01:35:05 +0300

    create app component

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,58 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { EshopSharedModule, UserRouteAccessService } from './shared';
+import { EshopHomeModule } from './home/home.module';
+import { EshopAdminModule } from './admin/admin.module';
+import { EshopAccountModule } from './account/account.module';
+import { EshopEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+import {HeaderComponent} from './layouts/header/header.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        EshopSharedModule,
+        EshopHomeModule,
+        EshopAdminModule,
+        EshopAccountModule,
+        EshopEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        HeaderComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent, HeaderComponent, NavbarComponent ]
+})
+export class EshopAppModule {}
---
+++ src/main/webapp/app/vendor.ts
@@ -0,0 +1,7 @@
+/* after changing this file run 'yarn run webpack:build:vendor' or 'yarn install' or 'yarn run webpack:build' */
+/* tslint:disable */
+import '../content/scss/vendor.scss';
+// import '../app/js/jquery-2.1.4.min.js';
+// import '../app/js/jquery.meanmenu.min.js';
+// import '../app/js/main.js';
+// jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
