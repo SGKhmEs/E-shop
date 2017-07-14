9f7b182eca6ad0be2c8aa211739343bc1b08ca4a
White28 <biluy2805@gmail.com>
2017-07-14 12:08:12 +0300

    Added checkoutAreaComponent

---
+++ src/main/webapp/app/layouts/checkout/checkoutArea/checkoutArea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'jhi-checkoutarea',
+    templateUrl: './checkoutArea.component.html',
+    styleUrls: [
+        './checkoutArea.css'
+    ]
+})
+export class CheckoutAreaComponent {}
