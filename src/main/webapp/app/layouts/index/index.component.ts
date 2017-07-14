be39415cbd821649eda3080d2acf6f8239369d7f
White28 <biluy2805@gmail.com>
2017-07-15 00:51:13 +0300

    Added indexComponent

---
+++ src/main/webapp/app/layouts/index/index.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'jhi-index',
+    templateUrl: './index.component.html'
+})
+export class IndexComponent {}
